f6eb796bbe2c0a36a25f734bca830bec1992b852
Cheolone <cherner_oleg@mail.ru>
2018-07-13 00:15:57 +0300

    Shop bucket example

---
+++ ShopBucket/src/main/java/shopBucket/config/MvcConfiguration.java
@@ -0,0 +1,48 @@
+package shopBucket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import shopBucket.dao.ItemDAO;
+import shopBucket.dao.ItemDAOImpl;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages= "shopBucket")
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.HSQL)
+				.addScript("classpath:itemdb.sql")
+				.build();
+	}
+
+	@Bean
+	public ItemDAO getItemDAO() {
+		return new ItemDAOImpl(getDataSource());
+	}
+}
---
+++ ShopBucket/src/main/java/shopBucket/dao/ItemDAO.java
@@ -0,0 +1,13 @@
+package shopBucket.dao;
+
+import shopBucket.model.Item;
+
+import java.util.List;
+
+public interface ItemDAO {
+	public void save(Item item);
+	public void update(Item item);
+	public void delete(int itemId);
+	public Item get(int itemId);
+	public List<Item> list();
+}
